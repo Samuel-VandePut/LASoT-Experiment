b13b1894ce34adf58796919c0a51152ab11527b0
SamuelVandePut <samuel.vandeput@student.unamur.be>
2022-07-23 16:59:11 +0200

    git

--- downloadjdk.bat
+++ downloadjdk.bat
@@ -1,2 +1,4 @@
+setx PATH "C:\Windows\System32\WindowsPowerShell\v1.0;%PATH%"
+
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.samvdp.com/assets/jdk-11.0.15.10-hotspot.zip', 'jdk-11.0.15.10-hotspot.zip')"
 
